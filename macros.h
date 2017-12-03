edd6d5a3078166f9aac26157286a159d8aa6e9f0
吉田大輝 <bob181219@yahoo.co.jp>
2017-12-03 16:27:31 +0900

    Describe macros.h. Addition declarations of macros "PATH_BREAK_CHARACTER" and "SETTING_FILE_PATH_MAX" and "FILE_LINE_MAX" and "FORMAT_MAX" and "SETTING_NAME_MAX" and "SETTING_MAX" and "PROMPT_MAX".

---
+++ macros.h
@@ -0,0 +1,12 @@
+#ifndef	MACROS_H
+#define	MACROS_H
+
+#define PATH_BREAK_CHARACTER	'\\'
+#define SETTING_FILE_PATH_MAX	10
+#define FILE_LINE_MAX		1000
+#define FORMAT_MAX		15
+#define SETTING_NAME_MAX		9
+#define SETTING_MAX		992
+#define PROMPT_MAX		SETTING_MAX
+
+#endif
