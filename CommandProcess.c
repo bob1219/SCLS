0b17adb7e7c61cae864421ef8ebeb626ac69b72c
吉田大輝 <bob181219@yahoo.co.jp>
2017-12-09 14:01:07 +0900

    Modify CommandProcess function. Addition process of a command "time".

--- CommandProcess.c
+++ CommandProcess.c
@@ -339,4 +339,16 @@ CommandProcess
 		
 		return 0;
 	}
+	else if(!strcmp(commands[0], "time"))
+	{
+		time_t	timer;
+		struct tm	*local;
+		
+		timer = time(NULL);
+		local = localtime(&timer);
+		
+		printf("%u:%u:%u\n", local -> tm_hour, local -> tm_min, local -> tm_sec);
+		
+		return 0;
+	}
 }
