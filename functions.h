dbd93f566e224fe327cc59357e1896d494dfe6c7
吉田大輝 <bob181219@yahoo.co.jp>
2017-12-12 21:04:23 +0900

    Modify functions.h. Addition declarations of function.

--- functions.h
+++ functions.h
@@ -29,8 +29,100 @@ const char*,
 int
 command_mdir
 (
-unsigned int,
+int,
+const char*
+);
+
+int
+command_rmdir
+(
+int,
+const char*
+);
+
+int
+command_rename
+(
+int,
+const char*,
+const char*
+);
+
+int
+command_chdir
+(
+int,
+const char*
+);
+
+int
+command_cudir
+(void);
+
+int
+command_mkfile
+(
+int,
+const char*
+);
+
+int
+command_rmfile
+(
+int,
+const char*
+);
+
+int
+command_cpfile
+(
+int,
+const char*,
+const char*
+);
+
+int
+command_lfile
+(
+int,
+const char*
+);
+
+int
+command_tview
+(
+int,
+const char*
+);
+
+int
+command_bview
+(
+int,
+const char*
+);
+
+int
+command_version
+(void);
+
+int
+command_app
+(
+int,
 const char**
 );
+
+int
+command_date
+(void);
+
+int
+command_time
+(void);
+
+int
+command_now
+(void);
 
 #endif
