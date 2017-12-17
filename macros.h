aa5ddbc818d517a3c3dfef582c722f24b0a3f321
吉田大輝 <bob181219@yahoo.co.jp>
2017-12-17 11:25:37 +0900

    Modify macros.h. Modify a macro "VERSION" to "1.2".

--- macros.h
+++ macros.h
@@ -8,7 +8,7 @@
 #define SETTING_NAME_MAX		9
 #define SETTING_MAX		992
 #define PROMPT_MAX		SETTING_MAX
-#define VERSION			"1.1"
+#define VERSION			"1.2"
 #define COMMAND_MAX		100
 #define COMMAND_NUMBER_MAX	11
 #define MONTH_MAX		10
